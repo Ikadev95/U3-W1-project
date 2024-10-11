86a1f0990a8886e29ca7b093b243f463f1fd2921
Ikadev95 <fegughi@gmail.com>
2024-10-11 11:54:20 +0200

    routing nav e footer ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path:'brand:name',
+    path:'brand/:brandName',
     component: BrandPageComponent
   },
   {
--- src/app/pages/brand-page/brand-page.component.ts
+++ src/app/pages/brand-page/brand-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iCar } from '../../Interfaces/i-car';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-brand-page',
   templateUrl: './brand-page.component.html',
   styleUrl: './brand-page.component.scss'
 })
-export class BrandPageComponent {
+export class BrandPageComponent implements OnInit {
+  cars: iCar[] = [];
+  paramsBrand!: string;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: any) => {
+      this.paramsBrand = params.brandName;
+      this.fetchCars();
+    });
+  }
+
+  fetchCars() {
+    fetch('./assets/db 2.json')
+      .then((res) => res.json())
+      .then((dati) => {
+        console.log(dati);
+        this.cars = dati.filter((car:iCar) => car.brand === this.paramsBrand);
+        console.log(this.cars);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
+
