0dc449f88dd828809e02b4295b10f21647a78ff1
Ikadev95 <fegughi@gmail.com>
2024-10-11 12:45:18 +0200

    loghi routing ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { iCar } from '../../Interfaces/i-car';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  cars:iCar[] = []
-  ngOnInit(){
-    fetch('./assets/db 2.json')
-    .then((res)=>res.json())
-    .then((dati)=>{
-      console.log(dati)
-      this.cars = dati
+export class HomeComponent implements OnInit {
+  cars: iCar[] = [];
+  imagesCar: { logoPath: string; brandName: string }[] = [];
 
-    })
-    .catch((err)=>{console.log(err)})
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    fetch('./assets/db 2.json')
+      .then((res) => res.json())
+      .then((dati) => {
+        console.log(dati);
+        this.cars = dati;
+
+        for (let i = 0; i < dati.length; i++) {
+          const brandLogo = dati[i].brandLogo;
+          const brandName = dati[i].brand;
+
+          const logoPath = `/assets/${brandLogo}`;
+
+          if (!this.imagesCar.some(image => image.logoPath === logoPath)) {
+            this.imagesCar.push({ logoPath, brandName });
+          }
+        }
+
+        console.log(this.imagesCar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  navigateToBrandPage(brandName: string) {
+    this.router.navigate(['/brand', brandName]);
   }
 }
